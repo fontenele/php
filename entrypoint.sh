c8b82b50bb1b4278e62b2cd9c2f7438bfc2cf8da
Guilherme Fontenele <guilherme@fontenele.net>
2023-04-23 16:37:20 -0300

    8.2 version

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ export PHP_SOCK_FILE=${PHP_SOCK_FILE:-'/run/php.sock'}
 export PHP_USER=${PHP_USER:-'www-data'}
 export PHP_GROUP=${PHP_GROUP:-'www-data'}
 export PHP_MODE=${PHP_MODE:-'0660'}
-export PHP_FPM_CONF=${PHP_FPM_CONF:-'/etc/php/8.0/fpm/php-fpm.conf'}
+export PHP_FPM_CONF=${PHP_FPM_CONF:-'/etc/php/8.2/fpm/php-fpm.conf'}
 
 envsubst '${NGINX_WEB_ROOT} ${NGINX_PHP_FALLBACK} ${NGINX_PHP_LOCATION} ${NGINX_USER} ${NGINX_CONF} ${PHP_SOCK_FILE} ${PHP_USER} ${PHP_GROUP} ${PHP_MODE} ${PHP_FPM_CONF}' < /tmp/nginx.conf.tpl > $NGINX_CONF
 envsubst '${NGINX_WEB_ROOT} ${NGINX_PHP_FALLBACK} ${NGINX_PHP_LOCATION} ${NGINX_USER} ${NGINX_CONF} ${PHP_SOCK_FILE} ${PHP_USER} ${PHP_GROUP} ${PHP_MODE} ${PHP_FPM_CONF}' < /tmp/php-fpm.conf.tpl > $PHP_FPM_CONF
